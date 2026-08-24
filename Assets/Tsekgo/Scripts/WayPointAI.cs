085843533b0d2c1edeb779e95ec59b210355291c
tsekgo <ngobesetsekgofatso@gamil.com>
2026-08-24 12:48:35 +0200

    Update on WayPoint script to cater specifically to luna

--- Assets/Tsekgo/Scripts/WayPointAI.cs
+++ Assets/Tsekgo/Scripts/WayPointAI.cs
@@ -25,9 +25,9 @@ public class WayPointAI : MonoBehaviour
 
     private void Awake()
     {
-        GameObject playerObj = GameObject.Find("PlayerObj");
-        if (playerObj != null)
-            player = playerObj.transform;
+        GameObject Luna = GameObject.Find("Luna");
+        if (Luna != null)
+            player = Luna.transform;
 
         agent = GetComponent<NavMeshAgent>();
     }
